150f164ddb3167a24f15ae2e9ebf232d07a94e3d
Khurram Aziz <Khurram Aziz>
2016-02-29 16:09:42 +0500

    Mirage + Ember Data

--- app/routes/invoices.js
+++ app/routes/invoices.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        // return [
+        //     { id: 1, type: 'invoice', customer: 'CUST', invoiceNumber: 'INV01', createDate: new Date(), posted: true, amount: 100 },
+        //     { id: 2, type: 'invoice', customer: 'CUST', invoiceNumber: 'INV02', createDate: new Date(), posted: true, amount: 110 }
+        // ];
+        return this.store.findAll('invoice');
+    }
 });
